237844e5213078561694c10235ecb16ebc3b69ee
rashaan <rmng.developer@gmail.com>
2025-06-02 19:53:34 -0400

    first a commit of new notion project

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
+
+function Header() {
+
+    const { user } = useUser();
+  return (
+    <div className="flex items-center justify-between p-5 bg-pink-300">
+        { user && <h1 className="text-2xl">{user?.firstName}{`'s`} Workstation</h1>}
+
+    
+
+    <div>
+        <SignedIn>
+                <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+                <SignInButton />
+        </SignedOut>
+    </div>
+    </div>
+  )
+}
+
+export default Header
