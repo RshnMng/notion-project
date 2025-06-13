eeef8d4bc2cc26de5aca2334482dc4db79e41980
rashaan <rmng.developer@gmail.com>
2025-06-13 10:42:27 -0400

    added arrow directional component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-        <h1>lets build some shit</h1>
-        <Button>lets gooooo!</Button>
-    </div>
+    <main className='flex space-x-2 items-center animate-pulse'>
+        <ArrowLeftCircle className="w-12 h-12" />
+        <h1 className="font-bold">Get started by creating a new document</h1>
+    </main>
   );
 }
